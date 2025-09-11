e9f349b2606a7e12f7efae48c9a678c6f544d8aa
dhigley6 <dhigley6@gmail.com>
2025-09-11 11:37:18 -0400

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://dhigley6.github.io',
+    base: '/daniel_homepage'
+});
