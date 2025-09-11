90dd44a6ce30f4c20a7d831b6443a3cf1ba646b8
dhigley6 <dhigley6@gmail.com>
2025-09-11 11:56:21 -0400

    Add components from tutorial

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://dhigley6.github.io',
-    base: '/daniel_homepage'
-});
